1e38ba17425a30e7043fff97e3ec1e2907aecafa
Giancarlo Ramirez <gecedeveloper@gmail.com>
2017-08-05 20:50:50 -0500

    Cambiando tipo de dato de precio a float

--- models/item.py
+++ models/item.py
@@ -5,7 +5,7 @@ class ItemModel(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
-	price = db.Column(db.Numeric(9,2))
+	price = db.Column(db.Float)
 
 	store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
 	store = db.relationship('StoreModel')
